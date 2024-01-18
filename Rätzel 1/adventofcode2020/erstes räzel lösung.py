0a5ddac76a555eb035c10321bb0b8ffacdaa4904
JanJasp <156820289+JanJasp@users.noreply.github.com>
2024-01-18 14:26:37 +0200

    2020

---
+++ Rätzel 1/adventofcode2020/erstes räzel lösung.py
@@ -0,0 +1,20 @@
+# Using readlines()
+# datei = open('day1.txt', 'r')
+datei = open('day1.txt', 'r')
+alleZeilen = datei.readlines()
+
+summe = 2020
+y = 0
+z = 0
+for zeile in alleZeilen:
+
+    intzahl:
+        x = zahl
+        for zahl in zeile:
+            if summe == x + zahl:
+                 z = zahl
+                 break
+            else:     
+                y += 1
+    print (x, zahl) 
+
